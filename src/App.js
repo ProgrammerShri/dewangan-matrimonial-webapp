3928122787c6d12f19df0b1b3bbf481232ec6dc6
ProgrammerShri <shrikantdewangan3@gmail.com>
2022-06-22 12:00:39 +0530

    apis data test

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Message from "./components/messages/Message";
@@ -13,6 +14,55 @@ import {
 } from "./routes/routes";
 
 function App() {
+  const [apisData, setApisData] = React.useState([]);
+  const [allData, setAllData] = React.useState([]);
+
+  React.useEffect(() => {
+    const urls = [
+      "https://jsonplaceholder.typicode.com/users",
+      "https://jsonplaceholder.typicode.com/todos",
+      "https://jsonplaceholder.typicode.com/posts",
+    ];
+
+    Promise.all([
+      fetch("https://jsonplaceholder.typicode.com/posts"),
+      fetch("https://jsonplaceholder.typicode.com/users"),
+    ])
+      .then(function (responses) {
+        // Get a JSON object from each of the responses
+        return Promise.all(
+          responses.map(function (response) {
+            return response.json();
+          })
+        );
+      })
+      .then(function (data) {
+        // Log the data to the console
+        // You would do something with both sets of data here
+        setApisData(data);
+      })
+      .catch(function (error) {
+        // if there's an error, log it
+      });
+  }, []);
+
+  React.useEffect(() => {
+    let tempArr = [];
+    apisData?.map((item) => {
+      console.log("Item", item);
+      item?.map((val) => {
+        const tempData = {
+          id: val?.id,
+          name: val?.name || val?.title,
+        };
+        tempArr.push(tempData);
+      });
+    });
+
+    setAllData(tempArr);
+  }, [apisData]);
+
+  console.log("DATA", allData);
   return (
     <>
       <BrowserRouter>
